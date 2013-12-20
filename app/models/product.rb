03f3dd85ce6067099835aaa590ce754e088215fd
yakkuru201 <tonamimagic@gmail.com>
2013-12-20 22:53:29 +0900

    商品とカートの関連付けを完成

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,21 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+    # この商品を参照している品目がないことを確認する
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, '品目が存在します')
+        return false
+      end
+    end
+
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
   validates :title, uniqueness: true
