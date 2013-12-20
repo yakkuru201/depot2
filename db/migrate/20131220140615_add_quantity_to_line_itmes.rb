51bc75577f0816719647aa023e73d49ad7222d28
yakkuru201 <tonamimagic@gmail.com>
2013-12-20 23:07:47 +0900

    デフォルトの値を１にした

--- db/migrate/20131220140615_add_quantity_to_line_itmes.rb
+++ db/migrate/20131220140615_add_quantity_to_line_itmes.rb
@@ -1,5 +1,5 @@
 class AddQuantityToLineItmes < ActiveRecord::Migration
   def change
-    add_column :line_itmes, :quantity, :integer
+    add_column :line_itmes, :quantity, :integer, default: 1
   end
 end
